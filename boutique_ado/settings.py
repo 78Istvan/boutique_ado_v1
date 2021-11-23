4dde814b766ad3afc77d5a7c345637b6f22a81c2
78Istvan <istvan78juhasz@gmail.com>
2021-11-23 00:06:32 +0000

    Fixed misspelling mistake so the test works on site

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -67,7 +67,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'temlates'),
+            os.path.join(BASE_DIR, 'templates'),
             os.path.join(BASE_DIR, 'templates', 'allauth'),
         ],
         'APP_DIRS': True,
